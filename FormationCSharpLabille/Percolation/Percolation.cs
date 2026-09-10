22639878abaa2595e2d13e4fe6b10723abd31b89
Formation <formation@intm.com>
2026-09-10 17:01:44 +0200

    Percoling en cours, je transperce la paroi poreuse

--- FormationCSharpLabille/Percolation/Percolation.cs
+++ FormationCSharpLabille/Percolation/Percolation.cs
@@ -27,27 +27,104 @@ namespace Percolation
 
         public bool IsOpen(int i, int j)
         {
-            return false;
+            return _open[i,j];
         }
 
         private bool IsFull(int i, int j)
         {
-            return false;
+            return _full[i, j];
         }
 
         public bool Percolate()
         {
+            for(int i = 0; i < _size ; i++)
+            {
+                if(_full[_size-1,i] == true)
+                {
+                    return true;
+                }
+            }
             return false;
         }
 
         private List<KeyValuePair<int, int>> CloseNeighbors(int i, int j)
         {
-            return null;
+            List<KeyValuePair<int, int>> Neighbours = new List<KeyValuePair<int,int>>();
+
+
+            // Vérification des cas de bord 
+            if( i > 0)
+            {
+                Neighbours.Add(new KeyValuePair<int, int>(i-1, j));
+            }
+            if (j > 0)
+            {
+                Neighbours.Add(new KeyValuePair<int, int>(i , j-1));
+            }
+            if (i < _size - 1)
+            {
+                Neighbours.Add(new KeyValuePair<int, int>(i + 1, j));
+            }
+            if (j < _size - 1)
+            {
+                Neighbours.Add(new KeyValuePair<int, int>(i , j+1));
+            }
+
+
+            return Neighbours;
         }
 
         public void Open(int i, int j)
         {
+            bool[,] LoopTreated = new bool[_size, _size];
+            init_loop(LoopTreated);
+            _open[i, j] = true;
 
+            List<KeyValuePair<int, int>> neighbours = CloseNeighbors(i, j);
+
+            //Check si un des voisins est rempli 
+            foreach (KeyValuePair<int,int> Coords in neighbours)
+            {
+                if (_full[Coords.Key, Coords.Value])
+                {
+                    _full[i, j] = true;
+                    LoopTreated[i, j] = true;
+                    break;
+                }
+            }
+
+            //Si on a rempli i , j , si voisins ouverts => rempli 
+            if (_full[i, j])
+            {
+                foreach (KeyValuePair<int, int> Coords in neighbours)
+                {
+                    LoopTreated[Coords.Key, Coords.Value] = true;
+
+                    if (_open[Coords.Key, Coords.Value])
+                    {
+                        _full[Coords.Key, Coords.Value] = true;
+                    }
+                }
+            }
+
+            
+
+            //
+
+
+            //
+
+        }
+
+        public void init_loop(bool[,] LoopTreated)
+        {
+            for(int i = 0; i < LoopTreated.GetLength(0); i ++)
+            {
+                for (int j =0; j < LoopTreated.GetLength(1); j++)
+                {
+                    LoopTreated[i, j] = false;
+                }
+            }
         }
     }
 }
