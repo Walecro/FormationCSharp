01459f57cd8f03574dc8f359a71d68076c466932
Formation <formation@intm.com>
2026-09-15 17:01:40 +0200

    Projet OR Avancement

---
+++ FormationCSharpLabille/Or/Business/TypeTransacConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Or.Business
+{
+    public class TypeTransacConverter : IValueConverter
+    {
+        private static readonly CultureInfo EuroCulture = new CultureInfo("fr-FR");
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is )
+            {
+                return value;
+            } 
+
+;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // Pas besoin en lecture seule
+        }
+    }
+}
